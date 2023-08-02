0eccce147770d575e6539bfc0be0acf5815196bb
keithlcheng <94764110+keithlcheng@users.noreply.github.com>
2023-08-02 18:26:19 -0400

    adding local backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [
-    tailwind(),
+    tailwind(), 
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
