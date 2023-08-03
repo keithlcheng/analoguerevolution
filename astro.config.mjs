af9adcec0c0a0289c760bba35d2312a5dac96c44
keithlcheng <94764110+keithlcheng@users.noreply.github.com>
2023-08-03 14:43:52 -0400

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
